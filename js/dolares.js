a1d06f72119af970a61f3bd2829d1857695b2c82
aurquiel <aurquiel@gmail.com>
2018-01-26 21:16:34 -0400

    This firts version send emails of quotation.

---
+++ js/dolares.js
@@ -0,0 +1,58 @@
+$(function() {
+
+    $("input,textarea").jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function($form, event, errors) {
+            // additional error messages or events
+        },
+        submitSuccess: function($form, event) {
+            event.preventDefault(); // prevent default submit behaviour
+            // get values from FORM
+            var namePaypal = document.getElementById("namePaypal").value;
+            var emailPaypal = document.getElementById("emailPaypal").value;
+            var dolaresPaypal = document.getElementById("dolaresPaypal").value;
+           
+            $.ajax({
+                url: "././mail/dolares.php",
+                type: "POST",
+                data: {
+                	  namePaypal: namePaypal,
+						  emailPaypal: emailPaypal,
+                    dolaresPaypal: dolaresPaypal                    
+                },
+                cache: false,
+                success: function() {
+                    // Success message
+
+                },
+                error: function() {
+                    // Fail message
+
+                },
+            })
+        },
+        filter: function() {
+            return $(this).is(":visible");
+        },
+    });
+
+    $("a[data-toggle=\"tab\"]").click(function(e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
+
+function calculoDolaresPaypal() {
+	var dolaresPaypal = document.getElementById("dolaresPaypal").value;
+	document.getElementById('paypalComision').innerHTML = (dolaresPaypal * 0.054) + 0.3;
+	document.getElementById('nuestraComisionPaypal').innerHTML = dolaresPaypal * 0.07;
+	
+	if (dolaresPaypal <= 0)
+	{
+        document.getElementById('bolivaresPaypal').innerHTML = 0;
+        document.getElementById('paypalComision').innerHTML = 0;
+	}    
+   else
+        document.getElementById('bolivaresPaypal').innerHTML = (dolaresPaypal - ((dolaresPaypal * 0.054) + 0.3 + (dolaresPaypal * 0.07)) ) * 261000;
+	
+}
---
+++ mail/dolares.php
@@ -0,0 +1,28 @@
+<?php
+// Check for empty fields
+if(empty($_POST['namePaypal'])  		||
+   empty($_POST['emailPaypal']) 		||
+   empty($_POST['dolaresPaypal'])	||
+   !filter_var($_POST['emailPaypal'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "No arguments Provided!";
+	return false;
+   }
+	
+$namePaypal = $_POST['namePaypal'];
+$emailPaypal = $_POST['emailPaypal'];
+$dolaresPaypal = $_POST['dolaresPaypal'];
+$paypalComision = ($dolaresPaypal*0.054)+0.3; 
+$nuestraComisionPaypal = $dolaresPaypal*0.007;
+$dolaresConvertir = ($dolaresPaypal - $paypalComision - $nuestraComisionPaypal);
+$bolivares =  $dolaresConvertir * 261000;
+	
+// Create the email and send the message
+$to = 'void@localhost'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Nueva Cotizacion Paypal de:  $namePaypal";
+$email_body = "Tienes una nueva cotizacion para cambiar dolares de Paypal.\n\n"."Estos son los detalles:\n\nNombre: $namePaypal\n\nEmail: $emailPaypal\n\nNeto Dolares: $dolaresPaypal $\n\nComision Paypal: $paypalComision\n\nComision Nuestra: $nuestraComisionPaypal\n\nSubtotal Dolares: $dolaresConvertir\n\nBolivares: $bolivares\n\n";
+$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $emailPaypal";	
+mail($to,$email_subject,$email_body,$headers);
+return true;			
+?>
